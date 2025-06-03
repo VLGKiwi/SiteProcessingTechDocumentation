c17da9d53f333df58f11684b21eb0938ede2b15a
VLGKiwi <zvonarev.dev@gmail.com>
2025-06-03 17:10:24 +0300

    feat: Artem/Clean-project: done

---
+++ frontend/src/modules/header/header.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import classNames from 'classnames'
+
+import styles from './header.module.scss'
+import { HeaderProps } from './header.types'
+import Logo from './logo'
+
+const Header: FC<HeaderProps> = ({ className }) => {
+  const headerClassName = classNames(styles.root, className)
+  return (
+    <header className={headerClassName}>
+      <Logo />
+    </header>
+  )
+}
+
+export default Header
